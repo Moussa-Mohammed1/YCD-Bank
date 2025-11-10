608f0cbdf5bf8a3d09f1125597a34381a97d1605
hamzazirari <hamzazirari6002@gmail.com>
2025-11-10 15:16:10 +0100

    UPDATE : update  virement page

---
+++ src/js/pages/beneficiaire.js
@@ -0,0 +1,14 @@
+const open_menu = document.getElementById("open-menu");
+const close_menu = document.getElementById("close-menu");
+const menu = document.getElementById("menu");
+
+import { openMenu } from "../utils/menuUtils.js";
+import { closeMenu } from "../utils/menuUtils.js";
+
+open_menu.addEventListener('click', ()=>{
+    openMenu(menu);
+});
+close_menu.addEventListener('click', ()=>{
+    menu.classList.remove("flex");
+    menu.classList.add("hidden");
+});
