8f45b8186fe36c2eb73a3239ce0240fb7064683a
hamzazirari <hamzazirari6002@gmail.com>
2025-11-14 10:16:35 +0100

    Finish modal in beneficiary page and finish virement page

--- src/js/pages/beneficiaire.js
+++ src/js/pages/beneficiaire.js
@@ -5,15 +5,112 @@ const menu = document.getElementById("menu");
 import { openMenu } from "../utils/menuUtils.js";
 import { closeMenu } from "../utils/menuUtils.js";
 
-open_menu.addEventListener('click', ()=>{
+open_menu.addEventListener('click', () => {
     openMenu(menu);
 });
-close_menu.addEventListener('click', ()=>{
-    menu.classList.remove("flex");
-    menu.classList.add("hidden");
+close_menu.addEventListener('click', () => {
+    closeMenu(menu);
 });
 
 
 
 // contenu beneficiare
 
+const btnAdd = document.getElementById("btn-add-beneficiaire");
+const modal = document.getElementById("modal-beneficiaire");
+const btnClose = document.getElementById("close-modal");
+
+btnAdd.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    modal.classList.add("flex");
+})
+
+btnClose.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    modal.classList.remove("flex");
+
+})
+
+const beneficiairecontainer = document.getElementById("benefeciaire");
+
+function creercarte(benefeciaire) {
+    const card = document.createElement("div")
+
+    card.innerHTML = `
+        <div id="card-${benefeciaire.id}" class="bg-white shadow-xl/30 rounded-lg p-4 flex justify-between items-center mb-4 relative">
+
+
+                             <!-- Bouton des 3 points -->
+
+         <button id="menu-btn-${benefeciaire.id}" class=" absolute top-2 right-2 text-gray-500">
+             <i class="fa-solid fa-ellipsis-vertical"></i>
+         </button>
+
+                                   <!-- Menu caché de 3 point-->
+
+                                   <div id="menu-${benefeciaire.id}"  class="menu-options  absolute top-8 right-2 bg-white shadow-lg rounded-lg border" style="display: none;">
+                                     <button onclick="bloquerDebloquer(${benefeciaire.id})" class="block w-full text-left px-4 py-2 hover:bg-gray-100">Bloquer/Debloquer</button>
+                                     <button onclick="supprimerCarte(${benefeciaire.id})" class="block w-full text-left px-4 py-2 hover:bg-gray-100">Bloquer/supprimer</button>
+                                   
+
+           <div>
+                    <p class="font-medium text-gray-800">${benefeciaire.nom}</p>
+                    <p class="text-sm text-gray-600">
+                        <span class="font-semibold">RIB</span> ${benefeciaire.rib}
+                    </p>
+                </div>
+
+
+                <div class="flex items-center gap-2">
+                    <span id="status-${benefeciaire.id}" class="bg-green-500 text-white text-xs px-3 py-1 rounded-full">
+                        Actif
+                    </span>
+                    <button class="bg-yellow-600 text-white p-2 rounded-lg">
+                        <i class="fa-solid fa-right-left"></i>
+                    </button>
+                </div>
+            </div >
+        `;
+
+    const btn = document.getElementById(`menu-btn-${benefeciaire.id}`);
+    const menu = document.getElementById(`menu-${benefeciaire.id}`);
+
+    btn.addEventListener("click", () => {
+        if (menu.style.display === "none") {
+            menu.style.display = "block";
+        } else {
+            menu.style.display = "none";
+        }
+    });
+
+    function bloquerDebloquer(id) {
+        const status = document.getElementById(`status-${id}`);
+
+        if (status.innerHTML === "Actif") {
+            status.innerHTML = "Bloqué";
+            status.classList.remove("bg-green-500");
+            status.classList.add("bg-red-500");
+        } else {
+            status.innerText = "Actif";
+            status.classList.remove("bg-red-500");
+            status.classList.add("bg-green-500");
+        }
+    }
+
+
+
+
+
+    beneficiairecontainer.appendChild(card);
+}
+
+function supprimerCarte(id) {
+    const confirmation = confirm("Voulez-vous vraiment supprimer ce bénéficiaire ?");
+    if (confirmation) {
+        const card = document.getElementById(`card-${id}`);
+        if (card) {
+            card.remove();
+        }
+        alert("Bénéficiaire supprimé avec succés !");
+    }
+}
